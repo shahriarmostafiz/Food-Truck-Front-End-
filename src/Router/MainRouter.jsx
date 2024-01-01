dad3410dd1c8523b19bc8486221c3f29ae97682d
ShahriarMostafiz <shahriarmostafiz19@gmail.com>
2024-01-01 22:17:55 +0600

    import issue

--- src/Router/MainRouter.jsx
+++ src/Router/MainRouter.jsx
@@ -1,20 +1,20 @@
 
 import { createBrowserRouter } from 'react-router-dom';
-import MainLayout from '../Layout/MainLAyout';
-import Home from '../Pages/Home/Home';
-import Login from '../Pages/userAuthentication/Login';
-import Register from '../Pages/userAuthentication/Register';
-import AllFoods from '../Pages/AllFoods/AllFoods';
+import MainLayout from '../Layout/MainLAyout.jsx';
+import Home from '../Pages/Home/Home.jsx';
+import Login from '../Pages/userAuthentication/Login.jsx';
+import Register from '../Pages/userAuthentication/Register.jsx';
+import AllFoods from '../Pages/AllFoods/AllFoods.jsx';
 // import { Axios } from 'axios';
-import SingleFood from '../Pages/SingleFood/SingleFood';
-import Purchase from '../Pages/Purchase/Purchase';
-import PriveRoute from './PriveRoute';
-import MyPurchase from '../Pages/MyPurchase/MyPurchase';
-import AddProduct from '../Pages/AddProduct/AddProduct';
-import MyAdded from '../Pages/MyAddedProduct/MyAdded';
-import UpdateProduct from '../Pages/UpdateMyProduct/UpdateProduct';
-import ErrorPage from '../Pages/ErrorPage/ErrorPage';
-import Blog from '../Pages/Blog/Blog';
+import SingleFood from '../Pages/SingleFood/SingleFood.jsx';
+import Purchase from '../Pages/Purchase/Purchase.jsx';
+import PriveRoute from './PriveRoute.jsx';
+import MyPurchase from '../Pages/MyPurchase/MyPurchase.jsx';
+import AddProduct from '../Pages/AddProduct/AddProduct.jsx';
+import MyAdded from '../Pages/MyAddedProduct/MyAdded.jsx';
+import UpdateProduct from '../Pages/UpdateMyProduct/UpdateProduct.jsx';
+import ErrorPage from '../Pages/ErrorPage/ErrorPage.jsx';
+import Blog from '../Pages/Blog/Blog.jsx';
 
 const MainRouter = createBrowserRouter([{
     path: "/",
